ec6774454f4368803a10274d8a087c49553de1b0
Moritz Wiesinger <momo201193@gmail.com>
2019-03-30 17:07:46 +0100

    Urls are getting logged when a willhaben tab is activated

--- background.js
+++ background.js
@@ -4,13 +4,30 @@
 
 'use strict';
 
+// check if an element exists in array using a comparer function
+// comparer : function(currentElement)
+Array.prototype.inArray = function(comparer) {
+  for(let i=0; i < this.length; i++) {
+    if(comparer(this[i])) return true;
+  }
+  return false;
+};
+
+// adds an element to the array if it does not already exist using a comparer
+// function
+Array.prototype.pushIfNotExist = function(element, comparer) {
+  if (!this.inArray(comparer)) {
+    this.push(element);
+  }
+};
+
 chrome.runtime.onInstalled.addListener(function () {
   chrome.storage.sync.set({willhaben_read_background_color: '#e0e0e0'}, () => {
     console.log('Successfully set new background color in storage.');
   });
 });
 
-chrome.tabs.onUpdated.addListener(
+chrome.tabs.onActivated.addListener(
   function (activeInfo) {
     chrome.tabs.query(
       {
@@ -22,24 +39,24 @@ chrome.tabs.onUpdated.addListener(
         if (tabs[0]) {
           let url = tabs[0].url;
           let product_url = url.match('willhaben\\.at([^?]*)')[0];
-          console.log("Current product url: " + product_url);
 
           chrome.storage.sync.get('urls', function (items) {
             let url_list = items['urls'];
-            console.log("url list: " + url_list);
-            if (url_list) {
-              url_list.push(product_url);
-            } else {
-              url_list = {};
+            console.log("Url list: ");
+            console.log(url_list);
+
+            if(url_list === undefined) {
+              url_list = [];
             }
+            else if(!Array.isArray(url_list)) {
+              chrome.storage.sync.remove('urls');
+              url_list = [];
+            }
+
+            url_list.pushIfNotExist(product_url, (e) => { return e === product_url });
             chrome.storage.sync.set({'urls': url_list});
-            console.log("Successfully set storage to: " + url_list);
           });
         }
-
-        chrome.storage.sync.getBytesInUse((bytesInUse => {
-          console.log("current storage usage: " + bytesInUse);
-        }));
       }
     );
   }
